6304d699cf66fe89235c735ff1b4b592f60ebde9
Divyansh Shukla <116350734+DivyanshShukla123@users.noreply.github.com>
2025-05-30 15:17:33 +0530

    exception.py

--- src/exception.py
+++ src/exception.py
@@ -28,5 +28,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
-#         logging.info("Logging has started")
+#         logging.info("Divide by zero")
 #         raise CustomException(e,sys)
