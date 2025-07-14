9331ff5e738d3350a08f0cb11d062d39e4acb6a0
Divyansh Shukla <divyanshshukla23may@gmail.com>
2025-07-14 20:13:51 +0530

    name

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='MLProject',
+    name='mlproject',
     version='0.0.1',
     author='Divyansh',
     author_email='divyanshshukla23may@gmail.com',
